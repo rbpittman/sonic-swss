5deb3713b59ba11a2727647375d7cd91063036be
Randall Pittman <randallbpittman@gmail.com>
2023-10-26 20:59:34 +0000

    No-op headroom check and buffer pool scripts.

---
+++ cfgmgr/buffer_check_headroom_cisco-8000.lua
@@ -0,0 +1,4 @@
+local result = {}
+table.insert(result, "result:true")
+table.insert(result, "debug: no headroom check operation to perform for Cisco-8000")
+return result
---
+++ cfgmgr/buffer_pool_cisco-8000.lua
@@ -0,0 +1,3 @@
+local result = {}
+table.insert(result, "debug: no buffer pool operation to perform for Cisco-8000")
+return result
